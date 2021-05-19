0dc8cbca633f38e7d06e1ee46175f111d1dbd9c7
Mariana Garcia <maridgarcia045@gmail.com>
2021-05-19 18:59:02 -0300

    Exercicio 5

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicios_dia1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicios_dia1.js
@@ -51,3 +51,23 @@ else if (valor < 0) {
 else {
   console.log('zero');
 }
+
+//5 Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+
+let num1 = 100;
+let num2 = -2;
+let num3 = 30;
+
+let somaDosAngulos = num1 + num2 +num3;
+let todosOsAngulos =  num1 > 0 && num2 > 0 && num3 > 0;
+
+if (todosOsAngulos) {
+    if (somaDosAngulos === 180) {
+        console.log(true);
+    }
+    else {
+        console.log(false);
+    };
+}else {
+        console.log('ângulo inválido');
+    }
