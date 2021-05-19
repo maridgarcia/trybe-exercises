c75d6db8ea307ddb130a9c0f8b08a1d3f496ca08
Mariana Garcia <maridgarcia045@gmail.com>
2021-05-19 18:46:04 -0300

    Exercicio 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicios_dia1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicios_dia1.js
@@ -28,12 +28,26 @@ let a = 4;
 let b = 12;
 let c = 10;
 
-if (a > b && a > c) {
-    console.log('a é o maior número');
+// if (a > b && a > c) {
+//     console.log('a é o maior número');
+// }
+// else if (b > a && b > c) {
+//   console.log('b é o maior número');
+// }
+// else {
+//     console.log('c é o maior número');
+// }
+
+//4 Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+let valor = 0;
+
+if (valor > 0) {
+  console.log('positivo');
 }
-else if (b > a && b > c) {
-  console.log('b é o maior número');
+else if (valor < 0) {
+  console.log('negativo');
 }
 else {
-    console.log('c é o maior número');
+  console.log('zero');
 }
