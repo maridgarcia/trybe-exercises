b73e7929300de22e4479b63f4e7302b5cae858ba
Mariana Garcia <maridgarcia045@gmail.com>
2021-05-20 17:52:30 -0300

    Exercicio 2

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios_dia2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios_dia2.js
@@ -5,3 +5,12 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index = 0; index < numbers.length; index+=1) {
     console.log(numbers[index]);
 }
+
+// 2 Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sumOfNumbers = 0;
+
+for (let index = 0; index <numbers.length; index+=1) {
+ sumOfNumbers+= numbers[index];
+}
+console.log(sumOfNumbers);
