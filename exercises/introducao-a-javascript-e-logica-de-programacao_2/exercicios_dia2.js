71d3684ea31a93bcc114e53150a74b4c8cd2ea15
Mariana Garcia <maridgarcia045@gmail.com>
2021-05-20 18:09:54 -0300

    Exercicio 3

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios_dia2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios_dia2.js
@@ -14,3 +14,15 @@ for (let index = 0; index <numbers.length; index+=1) {
  sumOfNumbers+= numbers[index];
 }
 console.log(sumOfNumbers);
+
+//3 Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+// console.log(numbers.length);
+
+for (let index = 0; index <numbers.length; index+=1) {
+    sum+= (numbers[index]) / numbers.length;
+   }
+
+//    let mediaAritmetica = sum / numbers.length;
+   console.log(sum);
