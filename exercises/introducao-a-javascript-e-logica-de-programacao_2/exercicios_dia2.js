e209382d5d1a931ce28b11f3e3df514cb2c24534
Mariana Garcia <maridgarcia045@gmail.com>
2021-05-20 19:05:08 -0300

    Exercicio 9

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios_dia2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicios_dia2.js
@@ -94,10 +94,10 @@ for (let index = 1; index <= 25; index += 1) {
 
 console.log(number);
 
-// let numbers = [];
+//9 Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
 
-// for (let index = 1; index <= 25; index += 1) {
-//   numbers.push(index);
-// }
+let numbers = [1,  2,  3,  4,  5,  6,  7,  8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,23, 24, 25];
 
-// console.log(numbers);
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index] / 2);
+  };
