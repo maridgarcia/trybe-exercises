0e1e81e35b92138e08f1ae2c180868734e72168f
Mariana Garcia <maridgarcia045@gmail.com>
2021-05-21 19:18:08 -0300

    Exercicio 2

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicios_dia3.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicios_dia3.js
@@ -1,6 +1,6 @@
 //1 Faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n .
 
-let n = 8;
+let n = 5;
 let linha = ' ';
 let asteriscos = '*';
 
@@ -8,12 +8,27 @@ let asteriscos = '*';
 for (let indexLinha = 0; indexLinha < n; indexLinha += 1) {
     linha = linha + asteriscos;
 }
-for (let indexLinha = 0; indexLinha < n; indexLinha += 1)
-console.log(linha);
-
-
+for (let indexLinha = 0; indexLinha < n; indexLinha += 1) {
+    console.log(linha);
+}
 
 //2 Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+
+let n = 5;
+let linha = '';
+let asteriscos = '*';
+
+for (let index = 0; index < n; index += 1) {
+    for (let index2 = 0; index2 < index + 1; index2 += 1) {
+      linha = linha + asteriscos;
+             
+  }
+  console.log(linha);
+  linha = '';
+  
+}
+
+
 
 //3 Agora inverta o lado do triângulo.
 
