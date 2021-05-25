aeddc3a310d5698f451da93222c01ecd6f4567f0
Mariana Garcia <maridgarcia045@gmail.com>
2021-05-25 13:51:01 -0300

    Exercicio 4 - Funcoes - Pt 2

--- introducao-a-javascript-e-logica-de-programcao_4/exercicios_dia4_pt2.js
+++ introducao-a-javascript-e-logica-de-programcao_4/exercicios_dia4_pt2.js
@@ -24,5 +24,46 @@ function largestNumber(number) {
   return biggerNumber;
 }
 
-
 console.log(largestNumber([2, 4, 6, 7, 10, 0, -3]));
+
+//3. Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+
+function largestNumber(number) {
+  let smallestNumber = 0;
+  for (let index in number) {
+    if (number[smallestNumber] > number[index]) {
+      smallestNumber = index;
+  }
+  }
+  return smallestNumber;
+}
+
+console.log(largestNumber([2, 4, 6, 7, 10, 0]));
+
+
+
+//4. Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+function maiorNome (names) {
+  let maiorNome = names[0];
+  for (let key in names) {
+      if (maiorNome.length < names[key].length) {
+          maiorNome = names[key];
+      }
+    }
+    return maiorNome;
+}
+
+console.log(maiorNome(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+
+function maiorPalavra(palavras) {
+  let maiorPalavra = palavras[0];
+  for (let indice in palavras) {
+    if (maiorPalavra.length < palavras[indice].length) {
+      maiorPalavra = palavras[indice];
+    }
+  }
+  return maiorPalavra;
+}
+
+console.log(maiorPalavra(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
