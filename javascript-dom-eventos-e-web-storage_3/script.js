24bdfe681645519aef04179a780e7b2a7f70d26b
Mariana Garcia <maridgarcia045@gmail.com>
2021-06-04 14:37:40 -0300

    Adiciona botao de Sexta-feira

--- javascript-dom-eventos-e-web-storage_3/script.js
+++ javascript-dom-eventos-e-web-storage_3/script.js
@@ -78,3 +78,17 @@ for (let index = 0; index < holiday.length; index += 1) {
 }
 
 showHolidays();
+
+// Implemente uma função que receba como parâmetro a string "Sexta-feira" e crie dinamicamente um botão com o nome "Sexta-feira". *
+// Adicione a este botão o ID "btn-friday" . *
+// Adicione este botão como filho/filha da tag <div> com classe "buttons-container" . *
+
+function friday(string) {
+  let fridayBtn = document.createElement('button');
+  fridayBtn.id = 'btn-friday';
+  fridayBtn.innerHTML = 'Sexta-feira';
+  btnContainer.appendChild(fridayBtn);
+}
+
+friday();
+
