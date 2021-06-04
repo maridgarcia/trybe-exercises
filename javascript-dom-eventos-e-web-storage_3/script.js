060aa4e43c124983a0c9c96d5851a5dc79d3ede4
Mariana Garcia <maridgarcia045@gmail.com>
2021-06-04 15:16:34 -0300

    altera conteudo dos dias ao clicar no botao sext-feira

--- javascript-dom-eventos-e-web-storage_3/script.js
+++ javascript-dom-eventos-e-web-storage_3/script.js
@@ -90,5 +90,44 @@ function friday(string) {
   btnContainer.appendChild(fridayBtn);
 }
 
-friday();
+friday('Sexta-feira');
 
+function clickFriday (fridayDays){
+  let fridayBtn = document.querySelector('#btn-friday');
+  let sextaFeira = document.querySelectorAll('.friday');
+  let sextaFeiraContent = 'Sexta-feira!';
+
+  fridayBtn.addEventListener('click', function(){
+    for (let index = 0; index < sextaFeira.length; index += 1) {
+      if(sextaFeira[index] !== sextaFeiraContent) {
+        sextaFeira[index].innerHTML = sextaFeiraContent;
+      } else {
+        sextaFeira[index].innerHTML = fridayDays[index];
+      }
+    }
+  })
+}
+
+let dezFridays = [4, 11, 18, 25];
+clickFriday(dezFridays);
+
+
+
+// function displayFridays(fridaysArray) {
+//   let getFridayButton = document.querySelector('#btn-friday');
+//   let fridays = document.getElementsByClassName('friday-day');
+//   let newFridayText = 'SEXTOU o/';
+
+//   getFridayButton.addEventListener('click', function() {
+//   for (let index = 0; index < fridays.length; index += 1) {
+//     if (fridays[index].innerHTML !== newFridayText) {
+//         fridays[index].innerHTML = newFridayText;
+//     } else {
+//         fridays[index].innerHTML = fridaysArray[index];
+//       }
+//     }
+//   })
+// };
+
+// let dezFridays = [ 4, 11, 18, 25 ];
+// displayFridays(dezFridays);
