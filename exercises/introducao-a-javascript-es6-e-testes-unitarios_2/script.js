fa445a138ed89fd44f1c4db7ae257b8cc2aceb66
Mariana Garcia <maridgarcia045@gmail.com>
2021-06-21 19:40:58 -0300

    Exercicio 5 - Parte II

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_2/script.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/script.js
@@ -91,3 +91,8 @@ console.log(length(lesson1));
 // Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 const value = (val) => Object.values(val);
 console.log(value(lesson1));
+
+// Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+
+const allLessons = Object.assign({},{ lesson1, lesson2, lesson3 });
+console.log(allLessons);
