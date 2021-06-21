63c841d880b54eba6c8dd317ddc7d7dd079a4cea
Mariana Garcia <maridgarcia045@gmail.com>
2021-06-21 19:27:06 -0300

    Exercicio 2 - Parte II

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_2/script.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/script.js
@@ -76,5 +76,10 @@ const order = {
 
   // Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
     const novoTurno = Object.assign(lesson2, {turno: 'manhã'});
-    // console.log(novoTurno);
     console.log(lesson2);
+
+  // Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+
+  const keys = (obj) => Object.keys(obj);
+
+  console.log(keys(lesson1));
