d5afc945a1a2d5b0e6bc56a7c174051a555519fd
Mariana Garcia <maridgarcia045@gmail.com>
2021-06-21 19:23:56 -0300

    Exercicio 1 - Parte II

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_2/script.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/script.js
@@ -52,3 +52,29 @@ const order = {
     
   
   console.log(orderModifier(order));
+
+
+  const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+  // Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+    const novoTurno = Object.assign(lesson2, {turno: 'manhã'});
+    // console.log(novoTurno);
+    console.log(lesson2);
