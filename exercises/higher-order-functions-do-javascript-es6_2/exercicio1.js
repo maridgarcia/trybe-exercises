848400994306c14627aac90ba3f83a42ef2e2789
Mariana Garcia <maridgarcia045@gmail.com>
2021-06-24 20:00:13 -0300

    Corrige linhas exercicio 1

--- exercises/higher-order-functions-do-javascript-es6_2/exercicio1.js
+++ exercises/higher-order-functions-do-javascript-es6_2/exercicio1.js
@@ -63,8 +63,6 @@ const books = [
   },
 ];
 
-// Adicione o código do exercício aqui:
-
 // 1. Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947. Dica: use a função find .
 
 function authorBornIn1947() {
