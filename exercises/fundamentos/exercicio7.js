ff9e3b902ff848f751c53025332f66be6f1c6620
Mariana Garcia <maridgarcia045@gmail.com>
2021-06-27 17:52:25 -0300

    Exercicio 7

--- exercises/fundamentos/exercicio7.js
+++ exercises/fundamentos/exercicio7.js
@@ -63,3 +63,17 @@ const books = [
     releaseYear: 1928,
   },
 ];
+
+// 7. Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+
+const expectedResult = 'O Senhor dos Anéis';
+
+function authorWith3DotsOnName() {
+  return books.find((book) => (
+    book.author.name.split(' ').filter((word) => word.endsWith('.')).length === 3
+  )).name; 
+}
+
+console.log(authorWith3DotsOnName());
+
+assert.deepStrictEqual(authorWith3DotsOnName(), expectedResult);
