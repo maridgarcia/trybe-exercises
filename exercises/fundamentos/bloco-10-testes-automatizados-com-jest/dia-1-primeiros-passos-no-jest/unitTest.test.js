f97c4cb9587759d83b3eda971c581c71c7bdaee4
Mariana Garcia <maridgarcia045@gmail.com>
2021-07-13 20:32:56 -0300

    Exercicios dia 10.01

---
+++ exercises/fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/unitTest.test.js
@@ -0,0 +1,16 @@
+const sum = require('./sum');
+
+//Teste se o retorno de sum(4, 5) é 9
+describe('test if the function sum', () => {
+  test('sum 4 + 5', () => {
+    expect(sum(4, 5)).toBe(9);
+  })
+  test('sum 0 + 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  })
+  test('throw error when a string is passed', () => {
+    expect(() => {
+      sum(4, "5");
+    }).toThrow()
+});
+})
